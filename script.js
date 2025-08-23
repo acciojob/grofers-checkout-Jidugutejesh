f90409d09b44ebdcd00b218d521c6b87f8294187
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-08-23 16:26:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const prices = document.querySelectorAll(".price")
+	let totalAmount =0;
+	for(let i =0;i<prices.length;i++){
+		totalAmount+=parseInt(prices[i].innerHTML)
+	}
+	const r0w=document.createElement('tr')
+	const td=document.createElement('td')
+	td.innerHTML=`TotalAmount ${totalAmount}`
+	td.colspan=2;
+	td.style.fontWeight='bold'
+
+	row.appendChild(td)
+	table.appenChild(row)
 };
 
 getSumBtn.addEventListener("click", getSum);
