5903ca3878b501ba8101318a224fa40efe60dcfa
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-08-23 16:40:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ const getSum = () => {
 	const prices = document.querySelectorAll(".price")
 	let totalAmount =0;
 	for(let i =0;i<prices.length;i++){
-		totalAmount+=parseInt(prices[i].innerText)
+		totalAmount+=parseFloat(prices[i].innerText)
 	}
+	const table = document.querySelector("table")
 	const row=document.createElement('tr')
 	const td=document.createElement('td')
-	td.innerHTML=`Total Amount:${totalAmount}`
+	td.innerHTML=`Total Amount: ${totalAmount}`
 	td.colspan=2;
 	td.style.fontWeight='bold'
 
