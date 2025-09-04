1384d137e3203e01d40edcca564bda2543bb60e5
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-09-04 13:29:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-  function calculateTotal() {
+ function calculateTotal() {
     let prices = document.querySelectorAll(".price");
     let total = 0;
 
     prices.forEach(cell => {
-      let val = cell.textContent.replace(/[^0-9]/g, "");
-      total += parseFloat(val) || 0;
+      let numbers = cell.textContent.match(/\d+/g);
+      let val = numbers ? parseInt(numbers[numbers.length - 1]) : 0;
+      total += val;
     });
 
     document.getElementById("ans").textContent = total;
